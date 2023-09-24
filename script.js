0115ad055ebd82eff748e6ceb3f708dd267e5aec
k02s <khsaleh022@gmail.com>
2023-09-24 17:16:21 +0300

    lab01

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// ask user their name 
+var username = prompt("Please, Enter Your Name:")
+alert("Weclome " + username + " !")
+
+// ask user about their age
+var age = prompt("Please, Enter Your Age:")
+
+// check if the age is less than 15
+if(age < 15){
+    console.log("You Can Not Login The Page")
+}else{
+    console.log("You Can Login The Page");
+}
